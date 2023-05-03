f42fd882fc8b2360c42c604424f93e81ddb56fe8
Samuel Thand <sath2102@student.miun.se>
2023-05-03 17:05:25 +0200

    Made basic layout for board component with dummy posts, made post model

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent {}
+export class BoardComponent {
+  protected posts: Post[] = [];
+
+  protected testPost: Post = {
+    _id: 'id123',
+    creator: 'Skapare skaparesson',
+    title: 'Titel',
+    content: 'blablablabla this is my post',
+    date: new Date(),
+    tag: 'TAG'
+  };
+
+  constructor() {
+    this.posts = Array.from({ length: 20 }, () => this.testPost);
+  }
+}
---
+++ src/app/models/post.model.ts
@@ -0,0 +1,8 @@
+export interface Post {
+  _id: string;
+  creator: string;
+  title: string;
+  content: string;
+  date: Date;
+  tag: string;
+}
