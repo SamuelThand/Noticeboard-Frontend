5bde5ddc553b204907156c5a547860264b4f2d9d
Samuel Thand <sath2102@student.miun.se>
2023-05-04 13:51:54 +0200

    Made post into a child component, added icons for buttons

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {}
+export class PostComponent {
+  @Input() post: Post = {
+    _id: '',
+    creator: '',
+    title: '',
+    content: '',
+    date: new Date(),
+    tag: ''
+  };
+}
