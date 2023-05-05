e5b26201b2681ef82524f1abc50bf2b4629e5c1a
Samuel Thand <sath2102@student.miun.se>
2023-05-05 14:42:06 +0200

    Built login component and user model

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected isPostFailed = false;
+  protected form: any = {
+    username: null,
+    password: null
+  };
+
+  login() {
+    // const username = this.form.username;
+    // const password = this.form.password;
+    // if (username && password) {
+    //   this.backendService.signIn(username, password).subscribe({
+    //     next: () => this.router.navigateByUrl('/admin-home'),
+    //     error: () => {
+    //       this.isPostFailed = true;
+    //     }
+    //   });
+    // }
+  }
+}
